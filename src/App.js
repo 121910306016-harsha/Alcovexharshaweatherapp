a59e2419c0533fbfb322540bff52b594c33deb19
121910306016 <121910306016@gitam.in>
2022-02-24 02:19:06 +0530

    done 23

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React, {useState, useEffect} from 'react';
+import "./main.css";
+import { fetchweather }from "./api/a";
+import Example from "./api/a";
+import axios from 'axios';
+import * as ReactBootstrap from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Spinner } from "reactstrap"
 
+const App = () => {
+  const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState({});
+  const [isLoading, setLoading] = useState(false);
+  const search = async (event) => {
+    if (event.key === "Enter") {
+      setLoading(true);
+      const data = await fetchweather(query);
+      setLoading(false);
+      setWeather(data);
+      setQuery("");
+    }
+    
+  };
+ let show;
+   show=  (
+   <div className="main-container">
+   <input
+     type="text"
+     className="search"
+     placeholder="Enter the city name"
+     value={query}
+     onChange={(event) => setQuery(event.target.value)}
+     onKeyPress={search}
+   />
+   {weather.main && (
+ 
+     <div className="city">
+       <h2 className="city-name">
+         <a>THE WEATHER APP</a><br></br>
+         <br></br>
+         <center><span>{weather.name}</span></center>
+       </h2>
+       <div className="city-temp">
+         {Math.round(weather.main.temp)}
+         <sup>&deg;C</sup>
+       </div>
+       <div className="info">
+         <img
+           className="city-icon"
+           src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+           alt={weather.weather[0].description}
+         />
+         <p>{weather.weather[0].description} </p>
+         
+       </div>
+     </div>
+   )}
+ </div>
+   );
+ 
+   return(<>{isLoading ?( <div>
+    <center><Spinner type="border" style={{ width: '2rem', height: '2rem'}}
+        children={false} /></center>
+</div>):(show) } </>)
+
+};
 export default App;
---
+++ src/api/a.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+import React, {useState, useEffect} from 'react';
+
+const URL = "https://api.openweathermap.org/data/2.5/weather";
+const API_KEY = "71f6779186cc32448b4c412eea65b982";
+var l=0
+export const fetchweather = async (query) => {
+  const { data } = await axios.get(URL, {
+    params: {
+      q: query,
+      units: "metric",
+      APPID: API_KEY,
+    },
+  });
+  return data;
+};
